ad7a08e34342a643fb9e63f360db305e6a8b20a5
LAPTOP-G1BCR7PC\Lenovo <17359672787@163.com>
2025-01-10 13:34:54 +0800

    相机跟随效果

--- scene/camera.gd
+++ scene/camera.gd
@@ -5,31 +5,42 @@
 #--------------------------------------
 class_name PlayerCamera extends Camera2D
 
-# TODO name ===============>信 号<===============
+# TODO 玩家相机 ===============>信 号<===============
 #region 信号
 
 #endregion
 
-# TODO name ===============>常 量<===============
+# TODO 玩家相机 ===============>常 量<===============
 #region 常量
 
 #endregion
 
-# TODO name ===============>变 量<===============
+# TODO 玩家相机 ===============>变 量<===============
 #region 变量
-
+var players : Array
 #endregion
 
-# TODO name ===============>虚方法<===============
+# TODO 玩家相机 ===============>虚方法<===============
 #region 常用的虚方法
 func _init() -> void:
 	pass
 
 func _ready() -> void:
-	pass
+	players = get_tree().get_nodes_in_group("Player")
+	for player in players:
+		if player is not Player:
+			players.erase(player)
 
 func _process(_delta: float) -> void:
-	pass
+	if players.size() == 1:
+		global_position = players[0].global_position
+	elif players.size() == 2:
+		global_position = (players[0].global_position + players[1].global_position) / 2
+
+		if players[0].global_position.distance_to(players[1].global_position) > 500:
+			zoom = lerp(zoom, Vector2.ONE * 2.5, .05)
+		else :
+			zoom = lerp(zoom, Vector2.ONE * 3, .05)
 
 func _physics_process(_delta: float) -> void:
 	pass
@@ -41,12 +52,12 @@ func _unhandled_input(_event: InputEvent) -> void:
 	pass
 #endregion
 
-# TODO name ===============>信号链接方法<===============
+# TODO 玩家相机 ===============>信号链接方法<===============
 #region 信号链接方法
 
 #endregion
 
-# TODO name ===============>工具方法<===============
+# TODO 玩家相机 ===============>工具方法<===============
 #region 工具方法
 
 #endregion
