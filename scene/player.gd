69e305dceb6e984287da5de0ba73434914e9049e
LAPTOP-G1BCR7PC\Lenovo <17359672787@163.com>
2025-01-10 13:03:07 +0800

    完成了玩家的移动、跳跃以及对应的动画

--- scene/player.gd
+++ scene/player.gd
@@ -5,22 +5,45 @@
 #--------------------------------------
 class_name Player extends CharacterBody2D
 
-# TODO name ===============>信 号<===============
+# TODO 玩家 ===============>信 号<===============
 #region 信号
 
 #endregion
 
-# TODO name ===============>常 量<===============
+# TODO 玩家 ===============>常 量<===============
 #region 常量
 
 #endregion
 
-# TODO name ===============>变 量<===============
+# TODO 玩家 ===============>变 量<===============
 #region 变量
+@export var speed : float
+@export var jump_speed : float
+@export var is_player_two : bool = false
+@export var jump_count : int = 2
 
+var dir : float:
+	set(v):
+		dir = v
+		if last_dir != dir and dir != 0:
+			last_dir = dir
+
+var last_dir : float:
+	set(v):
+		last_dir = v
+		if last_dir == -1:
+			rotation = 0
+			scale = Vector2(-1, 1)
+		elif last_dir == 1 :
+			rotation = 0
+			scale = Vector2(1, 1)
+
+var is_jump : bool = false
+
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
 #endregion
 
-# TODO name ===============>虚方法<===============
+# TODO 玩家 ===============>虚方法<===============
 #region 常用的虚方法
 func _init() -> void:
 	pass
@@ -29,10 +52,30 @@ func _ready() -> void:
 	pass
 
 func _process(_delta: float) -> void:
-	pass
+	dir = Input.get_axis("player_one_left", "player_one_right") if not is_player_two else Input.get_axis("player_two_left", "player_two_right")
+
+	is_jump = Input.is_action_just_pressed("player_one_jump") if not is_player_two else Input.is_action_just_pressed("player_two_jump")
+
+	if velocity.y < 0:
+		animation_player.play("jump")
+	elif velocity.y > 0:
+		animation_player.play("fall")
+	elif velocity.x != 0:
+		animation_player.play("run")
+	else :
+		animation_player.play("idle")
 
 func _physics_process(_delta: float) -> void:
-	velocity.y += 9.8
+	if is_on_floor():
+		jump_count = 2
+	else :
+		velocity.y += 9.8
+
+	if is_jump and jump_count > 0:
+		jump_count -= 1
+		velocity.y = -jump_speed
+
+	velocity.x = dir * speed
 	move_and_slide()
 
 func _input(_event: InputEvent) -> void:
@@ -42,12 +85,12 @@ func _unhandled_input(_event: InputEvent) -> void:
 	pass
 #endregion
 
-# TODO name ===============>信号链接方法<===============
+# TODO 玩家 ===============>信号链接方法<===============
 #region 信号链接方法
 
 #endregion
 
-# TODO name ===============>工具方法<===============
+# TODO 玩家 ===============>工具方法<===============
 #region 工具方法
 
 #endregion
